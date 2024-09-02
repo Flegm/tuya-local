b2b25e6e9fe16f88182a8f95aaa617443f4aaf91
y.karlykov <y@karlykov.ru>
2024-09-02 23:56:36 +0300

    Button default value edit 4 log

--- custom_components/tuya_local/button.py
+++ custom_components/tuya_local/button.py
@@ -57,5 +57,9 @@ class TuyaLocalButton(TuyaLocalEntity, ButtonEntity):
 
     async def async_press(self):
         """Press the button"""
-        value = "wall_follow"
+        _LOGGER.info(
+            "%s",
+            self._button_dp
+        )
+        value = None
         await self._button_dp.async_set_value(self._device, value if value else True)
